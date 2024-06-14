9c036c6f8eb649d8f4b8397ffecd729815077b4e
altamisatmaja <altamisfattah16@gmail.com>
2024-06-14 22:22:03 +0700

    fix: api gateway

--- ApiGateway/main.go
+++ ApiGateway/main.go
@@ -14,45 +14,64 @@ func merchantMiddle(next http.HandlerFunc) http.HandlerFunc {
 
 		if autor != "merchant" {
 			w.Write([]byte("Anda tidak memiliki akses"))
+			return
 		}
 
 		next.ServeHTTP(w, r)
 	}
 }
 
-func superAll(next http.HandlerFunc) http.HandlerFunc {
+func superMiddle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var autor = r.Header.Get("Authoritazion")
 
 		if autor != "su-admin" {
 			w.Write([]byte("Anda tidak memiliki akses"))
+			return
 		}
 
 		next.ServeHTTP(w, r)
 	}
 }
 
+type Merchant struct {
+	Nama          string `json:"nama`
+	NamaToko      string `json:"nama_toko`
+	jumlahProduct int    `json:"jumlah_product`
+}
+
+type Toko struct {
+	NamaToko      string `json:"nama_toko`
+	jumlahProduct int    `json:"jumlah_product`
+}
+
 func getMerchant(w http.ResponseWriter, r *http.Request) {
-	resp, _ := http.Get("http://localhost:8000/get-merchant-detail")
+	resp, _ := http.Get("http://localhost:8000/api/get-merchant-detail")
 
 	data, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(data))
+	var merch = &Merchant{}
+	json.Unmarshal(data, &merch)
 
-	json.NewEncoder(w).Encode(data)
+	json.NewEncoder(w).Encode(merch)
 }
 
 func getAllToko(w http.ResponseWriter, r *http.Request) {
-	resp, _ := http.Get("http://localhost:8000/get-all-toko")
+	resp, _ := http.Get("http://localhost:8000/api/get-all-toko")
 
 	data, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(data))
+	var toko = &[]Toko{}
+	json.Unmarshal(data, &toko)
 
-	json.NewEncoder(w).Encode(data)
+	json.NewEncoder(w).Encode(toko)
 }
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/merchant", merchantMiddle(getMerchant))
-	mux.HandleFunc("/toko", merchantMiddle(getAllToko))
+	mux.HandleFunc("/toko", superMiddle(getAllToko))
 
 	fmt.Println("Server is running")
 
